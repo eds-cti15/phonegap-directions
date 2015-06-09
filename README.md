76042653203e5bd8db43ecd28e924f8a087e16b3
Cassia <cassiasantos@eptc.prefpoa.com.br>
2015-06-09 17:08:47 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+# com.revivalx.cordova.geolocation
+This source code provides example about the device's location, such as latitude and longitude.
+
+##SETUP/USAGE/HOWTO
+Source code tutorial: http://blog.revivalx.com/2015/04/14/tutorial-geolocation-cordova-plugin-and-google-map-api-v3-for-ios-and-android/.
+
+##FAQ/CONTACT/TROUBLESHOOT
+Mohammad Nurdin bin Norazan
+
+- http://github.com/datomnurdin
+- http://twitter.com/datomnurdin
+- mohammadnrdn@gmail.com
---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
